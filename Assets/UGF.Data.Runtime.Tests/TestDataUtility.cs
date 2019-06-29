5209b9a111a0e2484455cf61a4cc5e5fa59e2f85
Alexander <vorobyovvorobyov@gmail.com>
2019-06-29 17:01:31 +0300

    Cleanup

--- Assets/UGF.Data.Runtime.Tests/TestDataUtility.cs
+++ Assets/UGF.Data.Runtime.Tests/TestDataUtility.cs
@@ -2,8 +2,5 @@
 {
     public class TestDataUtility
     {
-        public void Test()
-        {
-        }
     }
 }
--- Packages/UGF.Data/Runtime/IDataEnvironment.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UGF.Instance.Runtime;
-
-namespace UGF.Data.Runtime
-{
-    public interface IDataEnvironment : IInstanceCollection<Guid, IData>
-    {
-    }
-}
