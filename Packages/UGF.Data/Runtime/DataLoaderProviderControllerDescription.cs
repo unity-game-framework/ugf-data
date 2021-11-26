400f7e160017ac216e776e841d7d1f200854b8ea
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-11-26 19:30:51 +0300

    Add DataLoaderProviderController

---
+++ Packages/UGF.Data/Runtime/DataLoaderProviderControllerDescription.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UGF.Application.Runtime;
+using UGF.Builder.Runtime;
+using UGF.Module.Controllers.Runtime;
+
+namespace UGF.Data.Runtime
+{
+    public class DataLoaderProviderControllerDescription : ControllerDescription
+    {
+        public Dictionary<string, IBuilder<IApplication, IDataLoader>> Loaders { get; } = new Dictionary<string, IBuilder<IApplication, IDataLoader>>();
+    }
+}
