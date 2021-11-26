02c9a521ec57e8d084837257113822d51ea929ff
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-11-26 19:25:21 +0300

    Update DataLoaderFileTextAsset.cs

--- Packages/UGF.Data/Runtime/DataLoaderFileTextAsset.cs
+++ Packages/UGF.Data/Runtime/DataLoaderFileTextAsset.cs
@@ -1,4 +1,6 @@
-﻿using UGF.Application.Runtime;
+﻿using System.Text;
+using UGF.Application.Runtime;
+using UGF.RuntimeTools.Runtime.Encodings;
 using UnityEngine;
 
 namespace UGF.Data.Runtime
@@ -6,9 +8,15 @@ namespace UGF.Data.Runtime
     [CreateAssetMenu(menuName = "Unity Game Framework/Data/Data Loader File Text", order = 2000)]
     public class DataLoaderFileTextAsset : DataLoaderAsset
     {
+        [SerializeField] private EncodingType m_encoding = EncodingType.Default;
+
+        public EncodingType Encoding { get { return m_encoding; } set { m_encoding = value; } }
+
         protected override IDataLoader OnBuild(IApplication arguments)
         {
-            return new DataLoaderFileText();
+            Encoding encoding = EncodingUtility.GetEncoding(m_encoding);
+
+            return new DataLoaderFileText(encoding);
         }
     }
 }
