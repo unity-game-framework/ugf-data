dcae1cf563e6d5151e5b346d8999e68896b37063
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-11-26 19:19:14 +0300

    Add IDataLoader

---
+++ Packages/UGF.Data/Runtime/DataFileTextLoader.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using UGF.RuntimeTools.Runtime.Contexts;
+
+namespace UGF.Data.Runtime
+{
+    public class DataFileTextLoader : DataLoader
+    {
+        public Encoding Encoding { get; }
+
+        public DataFileTextLoader() : this(Encoding.Default)
+        {
+        }
+
+        public DataFileTextLoader(Encoding encoding)
+        {
+            Encoding = encoding ?? throw new ArgumentNullException(nameof(encoding));
+        }
+
+        protected override object OnRead(string path, IContext context)
+        {
+            return File.Exists(path) ? File.ReadAllText(path, Encoding) : string.Empty;
+        }
+
+        protected override async Task<object> OnReadAsync(string path, IContext context)
+        {
+            if (File.Exists(path))
+            {
+                await using var stream = new FileStream(path, FileMode.Open, FileAccess.Read);
+
+                byte[] bytes = new byte[stream.Length];
+
+                await stream.ReadAsync(bytes, 0, bytes.Length);
+
+                return Encoding.GetString(bytes);
+            }
+
+            return string.Empty;
+        }
+
+        protected override void OnWrite(string path, object data, IContext context)
+        {
+            if (data is not string content) throw new ArgumentException("Data must be type of String.");
+
+            CreateDirectoryForFile(path);
+
+            File.WriteAllText(path, content, Encoding);
+        }
+
+        protected override async Task OnWriteAsync(string path, object data, IContext context)
+        {
+            if (data is not string content) throw new ArgumentException("Data must be type of String.");
+
+            CreateDirectoryForFile(path);
+
+            byte[] bytes = Encoding.GetBytes(content);
+
+            await using var stream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+
+            await stream.WriteAsync(bytes, 0, bytes.Length);
+        }
+
+        private void CreateDirectoryForFile(string path)
+        {
+            string directoryPath = Path.GetDirectoryName(path);
+
+            if (!string.IsNullOrEmpty(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+        }
+    }
+}
---
+++ Packages/UGF.Data/Runtime/IDataLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using UGF.RuntimeTools.Runtime.Contexts;
+
+namespace UGF.Data.Runtime
+{
+    public interface IDataLoader
+    {
+        T Read<T>(string path, IContext context);
+        object Read(string path, IContext context);
+        Task<T> ReadAsync<T>(string path, IContext context);
+        Task<object> ReadAsync(string path, IContext context);
+        void Write<T>(string path, T data, IContext context);
+        void Write(string path, object data, IContext context);
+        Task WriteAsync<T>(string path, T data, IContext context);
+        Task WriteAsync(string path, object data, IContext context);
+    }
+}
