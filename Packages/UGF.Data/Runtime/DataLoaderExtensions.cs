160948d69cbab18e042e2d0d45bebaa071387002
Vorobey <vorobyovvorobyov@gmail.com>
2022-06-28 17:48:25 +0300

    Update DataLoaderExtensions.cs

--- Packages/UGF.Data/Runtime/DataLoaderExtensions.cs
+++ Packages/UGF.Data/Runtime/DataLoaderExtensions.cs
@@ -7,6 +7,18 @@ namespace UGF.Data.Runtime
 {
     public static class DataLoaderExtensions
     {
+        public static T Read<T>(this IDataLoader loader, string path, IContext context) where T : class
+        {
+            return (T)Read(loader, path, context);
+        }
+
+        public static object Read(this IDataLoader loader, string path, IContext context)
+        {
+            if (loader == null) throw new ArgumentNullException(nameof(loader));
+
+            return loader.TryRead(path, context, out object data) ? data : throw new ArgumentException($"{loader.GetType().Name} loader can't read from specified path: '{path}'.");
+        }
+
         public static bool TryRead<T>(this IDataLoader loader, string path, IContext context, out T data) where T : class
         {
             if (loader == null) throw new ArgumentNullException(nameof(loader));
@@ -32,7 +44,7 @@ namespace UGF.Data.Runtime
 
             TaskResult<object> result = await loader.TryReadAsync(path, context);
 
-            return result ? result.Value : throw new ArgumentException($"{loader.GetType().Name} can't read from specified path: '{path}'.");
+            return result ? result.Value : throw new ArgumentException($"{loader.GetType().Name} loader can't read from specified path: '{path}'.");
         }
 
         public static async Task<TaskResult<T>> TryReadAsync<T>(this IDataLoader loader, string path, IContext context) where T : class
@@ -41,7 +53,7 @@ namespace UGF.Data.Runtime
 
             TaskResult<object> result = await loader.TryReadAsync(path, context);
 
-            return (T)result.Value;
+            return result ? (T)result.Value : TaskResult<T>.Empty;
         }
 
         public static void Write(this IDataLoader loader, string path, object data, IContext context)
@@ -50,7 +62,7 @@ namespace UGF.Data.Runtime
 
             if (!loader.TryWrite(path, data, context))
             {
-                throw new ArgumentException($"{loader.GetType().Name} can't write at specified path: '{path}'.");
+                throw new ArgumentException($"{loader.GetType().Name} loader can't write at specified path: '{path}'.");
             }
         }
 
@@ -60,7 +72,7 @@ namespace UGF.Data.Runtime
 
             if (!await loader.TryWriteAsync(path, data, context))
             {
-                throw new ArgumentException($"{loader.GetType().Name} can't write at specified path: '{path}'.");
+                throw new ArgumentException($"{loader.GetType().Name} loader can't write at specified path: '{path}'.");
             }
         }
     }
