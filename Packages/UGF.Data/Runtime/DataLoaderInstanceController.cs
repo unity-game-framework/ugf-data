b7084d1c83faa94ee111baf16bf5e33b3de56755
Vorobey <vorobyovvorobyov@gmail.com>
2022-06-28 18:35:06 +0300

    Add DataLoaderInstanceController

---
+++ Packages/UGF.Data/Runtime/DataLoaderInstanceController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Threading.Tasks;
+using UGF.Application.Runtime;
+using UGF.Module.Controllers.Runtime;
+using UGF.RuntimeTools.Runtime.Tasks;
+
+namespace UGF.Data.Runtime
+{
+    public class DataLoaderInstanceController : ControllerDescribedAsync<DataLoaderInstanceControllerDescription>
+    {
+        public object Data { get { return m_data ?? throw new ArgumentException("Value not specified."); } }
+        public bool HasData { get { return m_data != null; } }
+
+        protected IDataLoaderController DataLoaderController { get; }
+
+        private object m_data;
+
+        public DataLoaderInstanceController(DataLoaderInstanceControllerDescription description, IApplication application) : base(description, application)
+        {
+            DataLoaderController = Application.GetController<IDataLoaderController>(Description.DataLoaderControllerId);
+        }
+
+        protected override void OnInitialize()
+        {
+            base.OnInitialize();
+
+            if (Description.ReadOnInitialize)
+            {
+                Read();
+            }
+        }
+
+        protected override async Task OnInitializeAsync()
+        {
+            await base.OnInitializeAsync();
+
+            if (Description.ReadOnInitializeAsync)
+            {
+                await ReadAsync();
+            }
+        }
+
+        protected override void OnUninitialize()
+        {
+            base.OnUninitialize();
+
+            if (Description.WriteOnUninitialize && HasData)
+            {
+                Write();
+            }
+        }
+
+        public T Get<T>() where T : class
+        {
+            return (T)Data;
+        }
+
+        public void Set(object data)
+        {
+            m_data = data ?? throw new ArgumentNullException(nameof(data));
+        }
+
+        public void Clear()
+        {
+            m_data = null;
+        }
+
+        public object Read()
+        {
+            return DataLoaderController.Read<object>(Description.Path);
+        }
+
+        public Task<object> ReadAsync()
+        {
+            return DataLoaderController.ReadAsync<object>(Description.Path);
+        }
+
+        public bool TryRead(out object data)
+        {
+            return DataLoaderController.TryRead(Description.Path, out data);
+        }
+
+        public Task<TaskResult<object>> TryReadAsync()
+        {
+            return DataLoaderController.TryReadAsync<object>(Description.Path);
+        }
+
+        public void Write()
+        {
+            DataLoaderController.Write(Description.Path, Data);
+        }
+
+        public Task WriteAsync()
+        {
+            return DataLoaderController.WriteAsync(Description.Path, Data);
+        }
+
+        public bool TryWrite()
+        {
+            return DataLoaderController.TryWrite(Description.Path, Data);
+        }
+
+        public Task<bool> TryWriteAsync()
+        {
+            return DataLoaderController.TryWriteAsync(Description.Path, Data);
+        }
+    }
+}
---
+++ Packages/UGF.Data/Runtime/DataLoaderInstanceControllerExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using UGF.RuntimeTools.Runtime.Tasks;
+
+namespace UGF.Data.Runtime
+{
+    public static class DataLoaderInstanceControllerExtensions
+    {
+        public static T Read<T>(this DataLoaderInstanceController controller) where T : class
+        {
+            if (controller == null) throw new ArgumentNullException(nameof(controller));
+
+            return (T)controller.Read();
+        }
+
+        public static async Task<T> ReadAsync<T>(this DataLoaderInstanceController controller) where T : class
+        {
+            if (controller == null) throw new ArgumentNullException(nameof(controller));
+
+            return (T)await controller.ReadAsync();
+        }
+
+        public static bool TryRead<T>(this DataLoaderInstanceController controller, out T data) where T : class
+        {
+            if (controller == null) throw new ArgumentNullException(nameof(controller));
+
+            if (controller.TryRead(out object value))
+            {
+                data = (T)value;
+                return true;
+            }
+
+            data = default;
+            return false;
+        }
+
+        public static async Task<TaskResult<T>> TryReadAsync<T>(this DataLoaderInstanceController controller) where T : class
+        {
+            if (controller == null) throw new ArgumentNullException(nameof(controller));
+
+            TaskResult<object> result = await controller.TryReadAsync();
+
+            return result ? (T)result.Value : TaskResult<T>.Empty;
+        }
+    }
+}
