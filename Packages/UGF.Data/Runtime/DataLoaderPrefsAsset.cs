4145688372290b152a2ae7cdf9b98c299a2b7457
Vorobey <vorobyovvorobyov@gmail.com>
2022-08-07 15:26:44 +0300

    Change DataLoaderPrefs

--- Packages/UGF.Data/Runtime/DataLoaderPrefsAsset.cs
+++ Packages/UGF.Data/Runtime/DataLoaderPrefsAsset.cs
@@ -6,9 +6,13 @@ namespace UGF.Data.Runtime
     [CreateAssetMenu(menuName = "Unity Game Framework/Data/Data Loader Prefs", order = 2000)]
     public class DataLoaderPrefsAsset : DataLoaderAsset
     {
+        [SerializeField] private bool m_saveOnWrite;
+
+        public bool SaveOnWrite { get { return m_saveOnWrite; } set { m_saveOnWrite = value; } }
+
         protected override IDataLoader OnBuild(IApplication arguments)
         {
-            return new DataLoaderPrefs();
+            return new DataLoaderPrefs(m_saveOnWrite);
         }
     }
 }
